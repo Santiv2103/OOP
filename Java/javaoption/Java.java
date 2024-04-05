a52cb4259c2fc82c9655d747b97414fa41d44e66
Santiv2103 <161525813+Santiv2103@users.noreply.github.com>
2024-04-04 19:10:09 -0500

    Add files via upload

---
+++ Java/javaoption/Java.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class Java {
+
+    public static void main(String[] args) {
+        String firstNumber;
+        String secondNumber;
+        int number1;
+        int number2 = 0;
+        int sum;
+        
+        firstNumber = JOptionPane.showInputDialog("Enter first integer");
+        
+        secondNumber = JOptionPane.showInputDialog("Enter second integer");
+        
+        number1 = Integer.parseInt(firstNumber);
+        number2 = Integer.parseInt(secondNumber);
+        
+        sum = number1 + number2;
+        
+        JOptionPane.showMessageDialog(null,"the sum is"+ sum, "Results",JOptionPane.PLAIN_MESSAGE);
+        
+         System.exit(0);
+
+    }
+    
+}
---
+++ Java/javaoption/Java_1.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JOptionPane;
+
+public class Java_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Welcome\nto\nJava\nProgramming!");
+        
+            System.exit(0);
+        
+        // TODO code application logic here
+    }
+    
+}
